9d1d16383876acf1a6533623e76ecb3b845adb31
MK <matthew.krecker@outlook.com>
2022-11-16 22:29:13 -0600

    initial project code

---
+++ ShellUtility/InfoCopier.cs
@@ -0,0 +1,32 @@
+﻿
+namespace ShellUtility
+{
+    public static class InfoCopier
+    {
+        public static int CopyInfo(string path, string action)
+        {
+            if (action == "path")
+            {
+                TextCopy.ClipboardService.SetText(path);
+                return 0;
+            }
+
+            if (action == "unix")
+            {
+                var drive = Path.GetPathRoot(path);
+                var relPath = Path.GetFullPath(path).Replace(drive, string.Empty).Replace('\\', '/');
+                TextCopy.ClipboardService.SetText($"/{drive[0]}/{relPath}");
+                return 0;
+            }
+
+            var file = new FileInfo(path);
+
+            if (!file.Exists) { return 1; }
+
+            TextCopy.ClipboardService.SetText(file.Name);
+            return 0;
+
+        }
+
+    }
+}
---
+++ ShellUtility/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ShellUtility
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+#if DEBUG
+            //InfoCopier.CopyInfo(args[0], args[1]);
+#endif
+
+            if (string.IsNullOrWhiteSpace(args[0])) { return 0; }
+
+            var retVal = 0;
+            var thread = new Thread(() => {
+                retVal = InfoCopier.CopyInfo(args[0], args[1]);
+            });
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+
+            return retVal;
+        }
+    }
+
+}
+
+
+
+/*
+ 
+HKEY_CLASSES_ROOT\*\shell\MK Copy File Name   C:\_prj\ShellUtility\publish\ShellUtility.exe "%1" name
+HKEY_CLASSES_ROOT\*\shell\MK Copy Full Path   C:\_prj\ShellUtility\publish\ShellUtility.exe "%1" path
+HKEY_CLASSES_ROOT\*\shell\MK Copy Unix Path   C:\_prj\ShellUtility\publish\ShellUtility.exe "%1" unix
+ 
+HKEY_CLASSES_ROOT\Directory\shell\MK Copy Folder Path   C:\_prj\ShellUtility\publish\ShellUtility.exe "%1" path
+HKEY_CLASSES_ROOT\Directory\shell\MK Copy Unix Path     C:\_prj\ShellUtility\publish\ShellUtility.exe "%1" unix
+ 
+*/
